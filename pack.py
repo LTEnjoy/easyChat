67cc043246459e285c65e9af03332e8d53c418cc
simon <59903527+Simon-Paul2017@users.noreply.github.com>
2025-06-20 08:42:35 +0800

    自动回复版本
    ui_auto_wechat里删掉了联系人限制，默认全部人都回复

--- pack.py
+++ pack.py
@@ -3,7 +3,8 @@ import os
 
 # 用来自动打包成exe程序
 def main():
-    cmd = f"pyinstaller.exe -Fw wechat_gui.py"
+    # cmd = f"pyinstaller.exe -Fw wechat_gui.py"
+    cmd = f"pyinstaller.exe -Fw wechat_gui_auto_replay.py"
 
     # 执行命令并打印输出
     result = os.system(cmd)
